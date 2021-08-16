549e115882bed76bb11f8b6a5bbac893513c7b7d
magali <magali.bensalah@gmail.com>
2021-08-16 11:40:53 +0200

    creation global

--- Pokemon RPG/Models/globals.swift
+++ Pokemon RPG/Models/globals.swift
@@ -4,5 +4,13 @@
 //
 //  Created by Magali Bensalah on 28/07/2021.
 //
+var playerOne = Player(name: "Joueur 1", characters: [])
+var playerTwo = Player(name: "Joueur 2", characters: [])
 
-import Foundation
+let playerOneWeapons: [Weapon] = [Weapon(name: "Gun", damage: 40),
+                                  Weapon(name: "Sword", damage: 15),
+                                  Weapon(name: "Knife", damage: 10)]
+
+let playerTwoWeapons: [Weapon] = [Weapon(name: "Knife", damage: 10),
+                                  Weapon(name: "Hammer", damage: 15),
+                                  Weapon(name: "Gun", damage: 40)]
